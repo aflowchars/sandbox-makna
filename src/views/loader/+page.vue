2abcbdd21df6d945983829ea367b3985caf35c29
Afochar Syabnirusmanto <aflowchars@gmail.com>
2023-04-10 15:21:16 +0700

    [update] loader

--- src/views/loader/+page.vue
+++ src/views/loader/+page.vue
@@ -1,9 +1,14 @@
 <script setup>
-import { Loader } from "@/components/atomics";
-import { Layout } from "@/components/templates";
+import { Loader } from '@/components/atomics'
+import { Layout } from '@/components/templates'
 </script>
 <template>
   <Layout title="Loader" desc="Ini adalah loading dari halaman">
-    <Loader />
+    <div class="flex flex-col items-center gap-16">
+      <Loader variant="icon-only" />
+      <div class="flex min-h-screen items-center justify-center">
+        <Loader variant="icon-text" />
+      </div>
+    </div>
   </Layout>
 </template>
